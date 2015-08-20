d58349ad4f364169f72bed039c36c761f9173f9f
chudaol <chudaol@gmail.com>
2015-08-20 13:41:42 +0200

    Update custom/js/custom.js

--- custom/js/custom.js
+++ custom/js/custom.js
@@ -1,4 +1,11 @@
 $(document).ready(function() {
+      var href;
+      
+      $("a.toggle-search").remove();
+      href = $(".custom-link").prop("href");
+      href = "http://" + _.last(href.split("-")) + ".ed-era.com";
+      $(".custom-link").prop("href", href);
+
 	$('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
 		disableOn: 700,
 		type: 'iframe',
@@ -8,4 +15,6 @@ $(document).ready(function() {
 
 		fixedContentPos: false
 	});
+	
+	
 });
