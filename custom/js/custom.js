32d4cedb995f138ba8fd963b29df7861b55e0197
chudaol <chudaol@gmail.com>
2015-09-09 14:41:49 +0200

    Update custom/js/custom.js

--- custom/js/custom.js
+++ custom/js/custom.js
@@ -1,21 +1,22 @@
 $(document).ready(function() {
-     var href;
+  var href, $lastLink;
   
-      $("a.toggle-search").remove();
-      href = $(".custom-link:last").prop("href");
-      //href = "https://ed-era.com/books/" + _.last(href.split("-"));
-      href = "http://" + _.last(href.split("-")) + ".ed-era.com";
-      $(".custom-link:last").prop("href", href);
+  $("a.toggle-search").remove();
+  
+  $lastLink = $(".custom-link:last");
+  href = $lastLink.prop("href");
+  href = "https://ed-era.com/books/" + _.last(href.split("-"));
+  $lastLink.prop("href", href);
 
-	$('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
-		disableOn: 700,
-		type: 'iframe',
-		mainClass: 'mfp-fade',
-		removalDelay: 160,
-		preloader: false,
-
-		fixedContentPos: false
-	});
+    $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+    	disableOn: 700,
+    	type: 'iframe',
+    	mainClass: 'mfp-fade',
+    	removalDelay: 160,
+    	preloader: false,
+    
+    	fixedContentPos: false
+    });
 	
 	
 });
