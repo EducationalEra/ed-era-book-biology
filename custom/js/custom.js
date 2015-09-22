2f105415b13cc0438a73033730362bf6cb7f98c4
Olga Filipova <chudaol@gmail.com>
2015-09-22 23:11:29 +0200

    added all dom manipulations to the page change event handler.

--- custom/js/custom.js
+++ custom/js/custom.js
@@ -1,22 +1,26 @@
 $(document).ready(function() {
-  var href, $lastLink;
-  
-  $("a.toggle-search").remove();
-  
-  $lastLink = $(".custom-link:last");
-  href = $lastLink.prop("href");
-  href = "https://ed-era.com/books/" + _.last(href.split("-"));
-  $lastLink.prop("href", href);
+  function init () {
+    var href, $lastLink;
+
+    $("a.toggle-search").remove();
+
+    $lastLink = $(".custom-link:last");
+    href = $lastLink.prop("href");
+    href = "https://ed-era.com/books/" + _.last(href.split("-"));
+    $lastLink.prop("href", href);
 
     $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
-    	disableOn: 700,
-    	type: 'iframe',
-    	mainClass: 'mfp-fade',
-    	removalDelay: 160,
-    	preloader: false,
-    
-    	fixedContentPos: false
+      disableOn: 700,
+      type: 'iframe',
+      mainClass: 'mfp-fade',
+      removalDelay: 160,
+      preloader: false,
+
+      fixedContentPos: false
     });
-	
-	
+  }
+  init();
+  require(["gitbook"], function (gitbook) {
+    gitbook.events.bind("page.change", init);
+  });
 });
